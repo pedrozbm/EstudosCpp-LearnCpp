939c026175e05a6b35a53d1b65d80af888f31c01
Pedro.barbieri <pedro.barbieri@grupoagtech.com.br>
2025-11-10 17:27:00 -0300

    inicio dos estudos de semantica do movimento

--- Cap.22/22.1_IntroPonteirosInteligentesSemanticaMovimento.cpp
+++ Cap.22/22.1_IntroPonteirosInteligentesSemanticaMovimento.cpp
@@ -2,16 +2,41 @@
 #include <memory>
 // Considere uma função na qual usamos alocação dinamica
 
+// void someFunction(){
+//     Resource* ptr = new Resource();
+
+//     int x;
+//     std::cout << "Entre com um int: ";
+//     std::cin >> x;
+
+//     if(x == 0)
+//         return; // Aqui a função retorna antecipadamente
+        // e o ponteiro não será excluído
+        // isso também pode ocorrer com throw
+
+    // A memoria alocada para o ponteiro será vazada
+    // E será vazada toda vez que essa função for chamada
+
+//     delete ptr;
+// }
+
+// Classes contém destruidores que são executados automaticamente quando 
+// um obj sai do escopo
+// Portanto podemos criar uma classe para ajudar a gerenciar e limpar os 
+// ponteiros! 
+
+
 template <typename T>
 class Auto_ptr2
 {
     T *m_ptr{};
 
 public:
+    // Construtuor moldado para possuir o ponteiro passado
     Auto_ptr2(T *ptr = nullptr) : m_ptr(ptr)
     {
     }
-
+    // O destruidor da classe garantirá que ele seja desalocado
     ~Auto_ptr2()
     {
         delete m_ptr;
@@ -33,10 +58,15 @@ public:
         a.m_ptr = nullptr;
         return *this;
     }
+
+    // Sobrecarga para poder usar Auto_ptr1 como m_ptr
     T &operator*() const { return *m_ptr; }
     T *operator->() const { return m_ptr; }
     bool isNull() const { return m_ptr == nullptr; }
 };
+// Essa classe é chamada de ponteiro inteligente.
+// Uma classe de composição é projetada para gerenciar a memoria 
+// e garantir que a memoria seja excluida ao objetos sairem do escopo
 
 class Resource
 {
@@ -48,6 +78,10 @@ public:
 int main()
 {
     Auto_ptr2<Resource> res1(new Resource());
+//    Observe a alocação de memória aqui
+// ... mas nenhuma exclusão explícita é necessária
+// Observe também que usamos <Resource>, não <Resource*>
+// Isso ocorre porque definimos m_ptr com o tipo T* (e não T)
     Auto_ptr2<Resource> res2;
 
     std::cout << "res1 is " << (res1.isNull() ? "null\n" : "not null\n");
